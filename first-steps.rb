332eee7947460afe43c059098f206d53468c9ffe
JRGCast <joao.ralph1@gmail.com>
2022-10-22 18:43:04 -0300

    added basic commands

---
+++ first-steps.rb
@@ -0,0 +1,9 @@
+# You can run a ruby script with the code runner or in terminal with ruby command. Use ruby --help for more instructions;
+# You also can use the Interactive Ruby - IRB with the command, works like the mongodb or python shell command
+puts "Hello world"
+print "Hello World"
+print " << See? No new line"
+puts " Jumping line"
+# The difference between puts and print is that puts acts like println (adds new line at the end) while print does not. 
+p 'this was written between single quotes'
+# The 'p' shows the string with the double quote marks
